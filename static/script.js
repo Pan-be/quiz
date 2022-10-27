dd2a9071692cd5febca36747fcecdee46a2728f6
Konrad <konradbieniek@gmail.com>
2022-10-27 16:08:27 +0200

    gerate game grid

--- static/script.js
+++ static/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", init)
 const eles = {}
 
+const myWords = [
+	"komunikatywność",
+	"Pracawgrupie",
+	"empatia",
+	"projektowanie",
+	"zarządzanie",
+]
+
+const game = { r: 9, c: 15, rxc: 135, x: "", y: "", arr: [] }
+
 function init() {
 	eles.gameArea = document.querySelector(".game-board")
 	eles.gridContainer = document.createElement("div")
@@ -13,4 +23,28 @@ function init() {
 	eles.gameArea.append(eles.myList)
 	eles.gameArea.append(eles.message)
 	eles.gameArea.append(eles.btn)
+	eles.btn.textContent = "Start the game."
+	eles.btn.addEventListener("click", startGame)
+}
+
+const startGame = () => {
+	console.log("game started")
+	game.arr.length = game.r * game.c
+	for (let index = 0; index < game.arr.length; index++) {
+		game.arr[index] = "-"
+	}
+
+	console.log(game)
+	eles.gridContainer.style.grid
+	buildBoard()
+}
+
+const buildBoard = () => {
+	eles.gridContainer.innerHTML = ""
+	game.arr.forEach((e, i) => {
+		let div = document.createElement("div")
+		div.textContent = e
+		div.classList.add("grid-item")
+		eles.gridContainer.append(div)
+	})
 }
