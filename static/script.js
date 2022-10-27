4bf8038091f93100e61d7eecf867a9ea1268b5bd
Konrad <konradbieniek@gmail.com>
2022-10-28 00:23:36 +0200

    insert words

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ const eles = {}
 
 const myWords = [
 	"komunikatywność",
-	"Pracawgrupie",
+	"pracawgrupie",
 	"empatia",
 	"projektowanie",
 	"zarządzanie",
@@ -34,9 +34,89 @@ const startGame = () => {
 		game.arr[index] = "-"
 	}
 
+	for (let ix = 0; ix < game.c; ix++) {
+		game.x += " auto "
+	}
+
+	for (let iy = 0; iy < game.r; iy++) {
+		game.y += " auto "
+	}
+
 	console.log(game)
-	eles.gridContainer.style.grid
+	eles.gridContainer.style.gridTemplateColumns = game.x
+	eles.gridContainer.style.gridTemplateRows = game.y
+
+	myWords.forEach((val, index) => {
+		let temp = placeWord(val)
+		console.log(temp)
+	})
+
 	buildBoard()
+}
+
+const placeWord = (word) => {
+	console.log(word)
+	let placedOk = false
+	let cnt = 300
+	word = word.split("")
+	console.log(word)
+	while (!placedOk && cnt > 0) {
+		cnt--
+		let pos = { col: 0, row: 0 }
+		let dir = (Math.random() > 0, 5) ? true : false
+		if (dir && word.length <= game.c) {
+			pos.col = findStartPos(word.length, game.c)
+			pos.row = Math.floor(Math.random() * game.r)
+
+			placedOk = xPlace(pos, word)
+		} else if (!dir && word.length <= game.r) {
+			pos.row = findStartPos(word.length, game.r)
+			pos.col = Math.floor(Math.random() * game.c)
+
+			placedOk = yPlace(pos, word)
+		}
+	}
+}
+const yPlace = (cor, word) => {
+	let start = cor.row * game.c + cor.col
+	let isOkCounter = 0
+	for (let i = 0; i < word.length; i++) {
+		if (game.arr[start + i * game.c] == "-") {
+			isOkCounter++
+		}
+	}
+	if (isOkCounter == word.length) {
+		for (let i = 0; i < word.length; i++) {
+			if (game.arr[start + i * game.c] == "-") {
+				game.arr[start + i * game.c] = word[i]
+			}
+		}
+		return true
+	}
+	return false
+}
+
+const xPlace = (cor, word) => {
+	let start = cor.row * game.c + cor.col
+	let isOkCounter = 0
+	for (let i = 0; i < word.length; i++) {
+		if (game.arr[start + i] == "-") {
+			isOkCounter++
+		}
+	}
+	if (isOkCounter == word.length) {
+		for (let i = 0; i < word.length; i++) {
+			if (game.arr[start + i] == "-") {
+				game.arr[start + i] = word[i]
+			}
+		}
+		return true
+	}
+	return false
+}
+
+const findStartPos = (wordVal, totalVal) => {
+	return Math.floor(Math.random() * (totalVal - wordVal + 1))
 }
 
 const buildBoard = () => {
